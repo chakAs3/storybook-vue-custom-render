da6d354e055438e8564e8b1c77e557c9ed76d9af
chakAs3 <javachakir@gmail.com>
2023-06-26 02:50:17 +0400

    first commit

---
+++ vue3-jsx/src/components/HelloWorld.tsx
@@ -0,0 +1,7 @@
+
+import { defineComponent } from 'vue';
+import MyButton from "./Button.vue"
+
+export default defineComponent({ render() { return <div>Hello JSX :<MyButton label='Helloooo button'></MyButton></div> }})
+
+// Path: vue3-jsx/src/main.ts
