f5f46d0a13730fe60aa512e0181dac91f243c32c
chakAs3 <javachakir@gmail.com>
2023-06-29 01:06:47 +0400

    add  story scope and component scope

---
+++ src/stories/JSXComp.tsx
@@ -0,0 +1,31 @@
+import { withModifiers, defineComponent, computed, ref } from 'vue';
+import './button.css'
+
+export const JSXComponent = defineComponent({
+  props: { label: String,counter: Number },  
+  name: 'JSXComponent',
+  emits: ['incrementCounter'],
+  setup(props , { emit }) {
+    
+    console.log('JSXComponent',props)
+    const counter = ref(props.counter ?? 0 ); 
+    const inc = () => {
+
+       
+        emit('incrementCounter', counter.value++ )
+    };
+
+    return () => (
+      <>
+       <div  class="storybook sb-column" >
+        <h4>JSX Component</h4>
+        <div class="sb-row">
+            <button class='storybook-button storybook-button--large' > { props.label } </button>  
+            <span class="storybook-button storybook-button--smal" onClick={withModifiers(inc, ['self'])} style="background:beige;padding:10px" > counter { counter.value } </span>
+            <p>shared state from the story</p>
+        </div>     
+       </div>
+      </>
+    );
+  },
+});
---
+++ src/stories/JSXComponent.stories.tsx
@@ -0,0 +1,74 @@
+import type { Meta, StoryObj, VueRenderer } from '@storybook/vue3';
+import type { DecoratorFunction } from "@storybook/types"
+import {  h } from "vue";
+
+import { JSXComponent } from './JSXComp';
+
+// More on how to set up stories at: https://storybook.js.org/docs/vue/writing-stories/introduction
+const decorators:DecoratorFunction<VueRenderer>[] =[]// [(storyFn)=> h('div',{style:{border:'2px solid red',padding:'20px'}} ,h(storyFn()) )]
+const meta = {
+  title: 'Example/JSXComponent',
+  component: JSXComponent,
+  // This component will have an automatically generated docsPage entry: https://storybook.js.org/docs/vue/writing-docs/autodocs
+  tags: ['autodocs'],
+  decorators
+} satisfies Meta<typeof JSXComponent>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+/*
+ *👇 Render functions are a framework specific feature to allow you control on how the component renders.
+ * See https://storybook.js.org/docs/vue/api/csf
+ * to learn how to use render functions.
+ */
+
+export const JSXSyntax: Story = {
+  args: {
+    label: ' Label from Story Args passed to JSXComponent',
+  },
+  render(args) {
+    return <JSXComponent {...args} />;
+  }
+};
+
+export const HRenderFunction: Story = {
+  args:{
+    label: ' Rendered Using h() function',
+  },
+  render(args) {
+    return h(JSXComponent, args);
+  },
+
+}
+
+export const CompositionApiComponent: Story = {
+  args:{
+    label: ' Rendered using Composition Component ',
+  },
+  render(args) {
+    return ({
+      components: { JSXComponent },
+      setup(props, { attrs }){
+        return { props , args , attrs}
+    }, 
+      template: `<pre>{{ JSON.stringify(args)}}</pre> 
+                 <JSXComponent v-bind="args"  />` 
+    });
+  }  
+}
+
+
+export const OptionsApiComponent: Story = {
+  args:{
+    label: ' Rendered usingComposition Component ',
+  },
+  render(args , { argTypes }) {
+    return ({
+      props: Object.keys(argTypes),
+      data: () => ({ args }),
+      components: { JSXComponent },
+      template: `<pre>{{ JSON.stringify(args)}}</pre><JSXComponent v-bind="args" />`
+    });
+  }  
+}
+
